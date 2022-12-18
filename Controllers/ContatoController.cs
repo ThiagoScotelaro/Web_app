da35b761c6bf061599e75f028cbcb9e941c975a7
Thiago Scotelaro <Thiago_scot@hotmail.com>
2022-12-18 17:27:08 -0300

    Iniciando_Front-End

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contact_Management_Web_application.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Apagarx()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/DetalhesContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contact_Management_Web_application.Controllers
+{
+    public class DetalhesContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
